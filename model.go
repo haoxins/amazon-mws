99beab17eb6fe4e8e7b4fd4b6e01409137556881
haoxin <haoxinst@gmail.com>
2020-08-19 16:00:45 +0800

    model init

---
+++ model.go
@@ -0,0 +1,73 @@
+package mws
+
+import "encoding/xml"
+
+// GetReportListResponse Get report list api response
+type GetReportListResponse struct {
+	XMLName             xml.Name            `xml:"GetReportListResponse"`
+	Text                string              `xml:",chardata"`
+	Xmlns               string              `xml:"xmlns,attr"`
+	GetReportListResult GetReportListResult `xml:"GetReportListResult"`
+	ResponseMetadata    struct {
+		Text      string `xml:",chardata"`
+		RequestID string `xml:"RequestId"`
+	} `xml:"ResponseMetadata"`
+}
+
+// GetReportListByNextTokenResponse The Get report list by next token response
+type GetReportListByNextTokenResponse struct {
+	XMLName             xml.Name            `xml:"GetReportListByNextTokenResponse"`
+	Text                string              `xml:",chardata"`
+	Xmlns               string              `xml:"xmlns,attr"`
+	GetReportListResult GetReportListResult `xml:"GetReportListResult"`
+	ResponseMetadata    struct {
+		Text      string `xml:",chardata"`
+		RequestID string `xml:"RequestId"`
+	} `xml:"ResponseMetadata"`
+}
+
+// GetReportListResult The Get report list result
+type GetReportListResult struct {
+	Text       string                `xml:",chardata"`
+	NextToken  string                `xml:"NextToken"`
+	HasNext    bool                  `xml:"HasNext"`
+	ReportInfo []SingleReportInfoRow `xml:"ReportInfo"`
+}
+
+// SingleReportInfoRow The report info row
+type SingleReportInfoRow struct {
+	Text            string `xml:",chardata"`
+	ReportType      string `xml:"ReportType"`
+	Acknowledged    string `xml:"Acknowledged"`
+	ReportID        string `xml:"ReportId"`
+	ReportRequestID string `xml:"ReportRequestId"`
+	AvailableDate   string `xml:"AvailableDate"`
+}
+
+// SettlementReportRow The settlement report row
+type SettlementReportRow struct {
+	SettlementID             string
+	SettlementStartDate      string
+	SettlementEndDate        string
+	DepositDate              string
+	TotalAmount              float64
+	Currency                 string
+	TransactionType          string
+	OrderID                  string
+	MerchantOrderID          string
+	AdjustmentID             string
+	ShipmentID               string
+	MarketplaceName          string
+	AmountType               string
+	AmountDescription        string
+	Amount                   float64
+	FulfillmentID            string
+	PostedDate               string
+	PostedDateTime           string
+	OrderItemCode            string
+	MerchantOrderItemID      string
+	MerchantAdjustmentItemID string
+	Sku                      string
+	QuantityPurchased        string
+	PromotionID              string
+}
