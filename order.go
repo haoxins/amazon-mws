9d65913fd6e11bd4422389dd9f3bc173191b7ede
haoxin <haoxinst@gmail.com>
2020-08-19 20:30:06 +0800

    Impl order

---
+++ order.go
@@ -0,0 +1,44 @@
+package mws
+
+import (
+	"net/url"
+	"time"
+
+	"github.com/pkg4go/tools"
+)
+
+// ListOrders List orders
+func (seller *Seller) ListOrders(countryCode string, startTime time.Time, endTime time.Time) {
+	params, err := seller.GenListOrdersParams(countryCode, startTime, endTime)
+	tools.AssertError(err)
+
+	seller.RequestOrder(params)
+}
+
+// GenListOrdersParams Generate list orders params
+func (seller *Seller) GenListOrdersParams(countryCode string, startTime time.Time, endTime time.Time) (string, error) {
+	v := url.Values{}
+
+	mid := MarketplaceID[countryCode]
+
+	v.Add("Action", "ListOrders")
+	v.Add("SellerId", seller.SellerID)
+	v.Add("MWSAuthToken", seller.AuthToken)
+	v.Add("AWSAccessKeyId", seller.AccessKey)
+	v.Add("MarketplaceId.Id.1", mid)
+	v.Add("LastUpdatedAfter", startTime.Format(time.RFC3339))
+	v.Add("LastUpdatedBefore", endTime.Format(time.RFC3339))
+	v.Add("Timestamp", time.Now().UTC().Format(time.RFC3339))
+	v.Add("SignatureVersion", "2")
+	v.Add("SignatureMethod", "HmacSHA256")
+	v.Add("Version", "2013-09-01")
+
+	s := v.Encode()
+
+	return seller.AddSignature(s), nil
+}
+
+// RequestOrder request order
+func (seller *Seller) RequestOrder(params string) ([]byte, error) {
+	return seller.Request(OrdersPath, params)
+}
