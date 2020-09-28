1416bfd82c61f1c0373815fda392af36c2c64f57
haoxin <haoxinst@gmail.com>
2020-09-28 18:36:05 +0800

    Add tests

--- finance_test.go
+++ finance_test.go
@@ -1,1 +1,26 @@
 package mws
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ListFinancialEvents(t *testing.T) {
+	s := &Seller{
+		Country:   os.Getenv("country"),
+		SellerID:  os.Getenv("seller_id"),
+		AuthToken: os.Getenv("auth_token"),
+		AccessKey: os.Getenv("access_key"),
+		SecretKey: os.Getenv("secret_key"),
+	}
+
+	assert.NotEqual(t, s.SellerID, "")
+
+	s.ListFinancialEvents(ListFinancialEventsParams{
+		PostedAfter:  time.Now().Add(-2 * time.Hour),
+		PostedBefore: time.Now().Add(-1 * time.Hour),
+	})
+}
--- fulfillment_inbound_shipment_test.go
+++ fulfillment_inbound_shipment_test.go
@@ -1,1 +1,35 @@
 package mws
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ListInboundShipments(t *testing.T) {
+	s := &Seller{
+		Country:   os.Getenv("country"),
+		SellerID:  os.Getenv("seller_id"),
+		AuthToken: os.Getenv("auth_token"),
+		AccessKey: os.Getenv("access_key"),
+		SecretKey: os.Getenv("secret_key"),
+	}
+
+	assert.NotEqual(t, s.SellerID, "")
+
+	// s.ListInboundShipments(ListInboundShipmentsParams{
+	// 	ShipmentStatusList: []shipmentStatus{
+	// 		shipmentStatusWorking,
+	// 		shipmentStatusShipped,
+	// 		shipmentStatusInTransit,
+	// 		shipmentStatusDelivered,
+	// 		shipmentStatusCheckedIn,
+	// 		shipmentStatusReceiving,
+	// 		shipmentStatusClosed,
+	// 		shipmentStatusDeleted,
+	// 	},
+	// 	LastUpdatedAfter:  time.Now().Add(-2 * time.Hour),
+	// 	LastUpdatedBefore: time.Now().Add(-1 * time.Hour),
+	// })
+}
