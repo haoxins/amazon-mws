5fe05e7867ab8930d624d7df9cc131367aceecd9
haoxin <haoxinst@gmail.com>
2020-09-28 18:36:05 +0800

    Add tests

--- feed_test.go
+++ feed_test.go
@@ -1,1 +1,25 @@
 package mws
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GetFeedSubmissionList(t *testing.T) {
+	s := &Seller{
+		Country:   os.Getenv("country"),
+		SellerID:  os.Getenv("seller_id"),
+		AuthToken: os.Getenv("auth_token"),
+		AccessKey: os.Getenv("access_key"),
+		SecretKey: os.Getenv("secret_key"),
+	}
+
+	assert.NotEqual(t, s.SellerID, "")
+
+	// s.GetFeedSubmissionList(GetFeedSubmissionListParams{
+	// 	SubmittedFromDate: time.Now().Add(-8 * 24 * time.Hour),
+	// 	SubmittedToDate:   time.Now().Add(-1 * time.Hour),
+	// })
+}
