f7e45995faaa54c815d9cca3f95b7a13ba5daab2
haoxin <haoxinst@gmail.com>
2020-10-12 21:31:12 +0800

    Add ErrorResponse

--- common_model.go
+++ common_model.go
@@ -1,7 +1,23 @@
 package mws
 
+import "encoding/xml"
+
 // ResponseMetadata ...
 type ResponseMetadata struct {
 	Text      string `xml:",chardata"`
 	RequestID string `xml:"RequestId"`
+}
+
+// ErrorResponse ...
+type ErrorResponse struct {
+	XMLName xml.Name `xml:"ErrorResponse"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Error   struct {
+		Text    string `xml:",chardata"`
+		Type    string `xml:"Type"`
+		Code    string `xml:"Code"`
+		Message string `xml:"Message"`
+	} `xml:"Error"`
+	RequestID string `xml:"RequestID"`
 }
