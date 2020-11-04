40d426fdeed6ec210059ee2cc826481c136db372
haoxin <haoxinst@gmail.com>
2020-11-04 21:53:09 +0800

    trim

--- inventory_model.go
+++ inventory_model.go
@@ -5,7 +5,6 @@ import "encoding/xml"
 // ListInventorySupplyResponse ...
 type ListInventorySupplyResponse struct {
 	XMLName                   xml.Name                  `xml:"ListInventorySupplyResponse"`
-	Text                      string                    `xml:",chardata"`
 	Xmlns                     string                    `xml:"xmlns,attr"`
 	ListInventorySupplyResult ListInventorySupplyResult `xml:"ListInventorySupplyResult"`
 	ResponseMetadata          ResponseMetadata          `xml:"ResponseMetadata"`
@@ -14,7 +13,6 @@ type ListInventorySupplyResponse struct {
 // ListInventorySupplyByNextTokenResponse ...
 type ListInventorySupplyByNextTokenResponse struct {
 	XMLName                   xml.Name                  `xml:"ListInventorySupplyByNextTokenResponse"`
-	Text                      string                    `xml:",chardata"`
 	Xmlns                     string                    `xml:"xmlns,attr"`
 	ListInventorySupplyResult ListInventorySupplyResult `xml:"ListInventorySupplyByNextTokenResult"`
 	ResponseMetadata          ResponseMetadata          `xml:"ResponseMetadata"`
@@ -22,7 +20,6 @@ type ListInventorySupplyByNextTokenResponse struct {
 
 // ListInventorySupplyResult ...
 type ListInventorySupplyResult struct {
-	Text                string              `xml:",chardata"`
 	MarketplaceID       string              `xml:"MarketplaceId"`
 	InventorySupplyList InventorySupplyList `xml:"InventorySupplyList"`
 	NextToken           string              `xml:"NextToken"`
@@ -30,18 +27,15 @@ type ListInventorySupplyResult struct {
 
 // InventorySupplyList ...
 type InventorySupplyList struct {
-	Text    string                  `xml:",chardata"`
 	Members []InventorySupplyMember `xml:"member"`
 }
 
 // InventorySupplyMember ...
 type InventorySupplyMember struct {
-	Text                 string `xml:",chardata"`
 	Condition            string `xml:"Condition"`
 	SupplyDetail         string `xml:"SupplyDetail"`
 	TotalSupplyQuantity  string `xml:"TotalSupplyQuantity"`
 	EarliestAvailability struct {
-		Text          string `xml:",chardata"`
 		TimepointType string `xml:"TimepointType"`
 		DateTime      string `xml:"DateTime"`
 	} `xml:"EarliestAvailability"`
