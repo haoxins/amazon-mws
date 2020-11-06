ed66da3f7c717143dac9470a77c4b0e158a79688
haoxin <haoxinst@gmail.com>
2020-11-06 15:16:03 +0800

    Split and rename

--- feed_submission_list.go
+++ feed_submission_list.go
@@ -3,39 +3,11 @@ package mws
 import (
 	"encoding/xml"
 	"errors"
-	"fmt"
-	"io/ioutil"
 	"net/url"
 	"time"
 
 	"github.com/haoxins/tools/v2"
 )
-
-// GetFeedSubmissionResult ...
-func (seller *Seller) GetFeedSubmissionResult(feedSubmissionID string) {
-	qs := seller.genGetFeedSubmissionResultParams(feedSubmissionID)
-
-	body, err := seller.get(FeedsPath, qs)
-	tools.PanicError(err)
-	// WTF - TODO - xlsx, xml, txt
-	fname := tools.Resolve(tools.Getwd(), fmt.Sprintf("feed_submission_result_%s.xlsx", feedSubmissionID))
-	err = ioutil.WriteFile(fname, body, 0644)
-	tools.PanicError(err)
-}
-
-func (seller *Seller) genGetFeedSubmissionResultParams(feedSubmissionID string) string {
-	v := url.Values{}
-
-	seller.addBasicParams(&v)
-
-	v.Add("Action", "GetFeedSubmissionResult")
-	v.Add("Version", "2009-01-01")
-	v.Add("FeedSubmissionId", feedSubmissionID)
-
-	s := v.Encode()
-
-	return s
-}
 
 // GetFeedSubmissionListParams MWS GetFeedSubmissionList API params
 type GetFeedSubmissionListParams struct {
