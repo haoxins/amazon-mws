9a0ad298ae15464bd514cbf9d1ed545b3497762b
haoxin <haoxinst@gmail.com>
2020-11-09 11:53:11 +0800

    Add ListInboundShipmentItems

--- inbound_shipment_item_test.go
+++ inbound_shipment_item_test.go
@@ -1,1 +1,28 @@
 package mws
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ListInboundShipmentItems(t *testing.T) {
+	s := &Seller{
+		Country:   os.Getenv("country"),
+		SellerID:  os.Getenv("seller_id"),
+		AuthToken: os.Getenv("auth_token"),
+		AccessKey: os.Getenv("access_key"),
+		SecretKey: os.Getenv("secret_key"),
+	}
+
+	assert.NotEqual(t, s.SellerID, "")
+
+	// members := s.ListInboundShipmentItems(ListInboundShipmentItemsParams{
+	// 	ShipmentID:        "",
+	// 	LastUpdatedAfter:  time.Now().Add(-24 * time.Hour),
+	// 	LastUpdatedBefore: time.Now().Add(-1 * time.Hour),
+	// })
+
+	// pp.Printf("Members %+v", members)
+}
