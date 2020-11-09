4ffe229018448833e3f209e034734810ae6e7900
haoxin <haoxinst@gmail.com>
2020-11-09 11:53:11 +0800

    Add ListInboundShipmentItems

---
+++ inbound_shipment_item.go
@@ -0,0 +1,101 @@
+package mws
+
+import (
+	"encoding/xml"
+	"errors"
+	"net/url"
+	"time"
+
+	"github.com/haoxins/tools/v2"
+)
+
+// ListInboundShipmentItemsParams ListInboundShipmentItems API params
+type ListInboundShipmentItemsParams struct {
+	ShipmentID        string
+	LastUpdatedAfter  time.Time
+	LastUpdatedBefore time.Time
+}
+
+// ListInboundShipmentItems ...
+func (seller *Seller) ListInboundShipmentItems(params ListInboundShipmentItemsParams) []InboundShipmentItemMember {
+	qs := seller.genListInboundShipmentItemsParams(params, "")
+
+	var members []InboundShipmentItemMember
+
+	result, err := seller.requestInboundShipmentItems(qs, false)
+	tools.PanicError(err)
+
+	members = append(members, result.ItemData.Members...)
+
+	for {
+		if result.NextToken == "" {
+			break
+		}
+
+		result = seller.listInboundShipmentItemsByNextToken(result.NextToken)
+		members = append(members, result.ItemData.Members...)
+	}
+
+	return members
+}
+
+func (seller *Seller) listInboundShipmentItemsByNextToken(nextToken string) ListInboundShipmentItemsResult {
+	qs := seller.genListInboundShipmentItemsParams(ListInboundShipmentItemsParams{}, nextToken)
+
+	result, err := seller.requestInboundShipmentItems(qs, true)
+	tools.PanicError(err)
+
+	return result
+}
+
+func (seller *Seller) genListInboundShipmentItemsParams(params ListInboundShipmentItemsParams, nextToken string) string {
+	v := url.Values{}
+
+	seller.addBasicParams(&v)
+
+	v.Add("Version", "2010-10-01")
+
+	if params.ShipmentID != "" {
+		v.Add("ShipmentId", params.ShipmentID)
+
+	} else {
+		v.Add("LastUpdatedAfter", params.LastUpdatedAfter.Format(time.RFC3339))
+		v.Add("LastUpdatedBefore", params.LastUpdatedBefore.Format(time.RFC3339))
+	}
+
+	if nextToken != "" {
+		v.Add("Action", "ListInboundShipmentItemsByNextToken")
+		v.Add("NextToken", nextToken)
+	} else {
+		v.Add("Action", "ListInboundShipmentItems")
+	}
+
+	s := v.Encode()
+
+	return s
+}
+
+func (seller *Seller) requestInboundShipmentItems(qs string, byNextToken bool) (ListInboundShipmentItemsResult, error) {
+	body, err := seller.get(InboundShipmentPath, qs)
+	tools.PanicError(err)
+
+	if byNextToken {
+		data := ListInboundShipmentItemsByNextTokenResponse{}
+		err = xml.Unmarshal(body, &data)
+
+		if err != nil {
+			return data.ListInboundShipmentItemsResult, errors.New(string(body))
+		}
+
+		return data.ListInboundShipmentItemsResult, nil
+	}
+
+	data := ListInboundShipmentItemsResponse{}
+	err = xml.Unmarshal(body, &data)
+
+	if err != nil {
+		return data.ListInboundShipmentItemsResult, errors.New(string(body))
+	}
+
+	return data.ListInboundShipmentItemsResult, nil
+}
--- inbound_shipment_item_model.go
+++ inbound_shipment_item_model.go
@@ -1,1 +1,46 @@
 package mws
+
+import "encoding/xml"
+
+// ListInboundShipmentItemsResponse ...
+type ListInboundShipmentItemsResponse struct {
+	XMLName                        xml.Name                       `xml:"ListInboundShipmentItemsResponse"`
+	Xmlns                          string                         `xml:"xmlns,attr"`
+	ListInboundShipmentItemsResult ListInboundShipmentItemsResult `xml:"ListInboundShipmentItemsResult"`
+	ResponseMetadata               ResponseMetadata               `xml:"ResponseMetadata"`
+}
+
+// ListInboundShipmentItemsByNextTokenResponse ...
+type ListInboundShipmentItemsByNextTokenResponse struct {
+	XMLName                        xml.Name                       `xml:"ListInboundShipmentItemsByNextTokenResponse"`
+	Xmlns                          string                         `xml:"xmlns,attr"`
+	ListInboundShipmentItemsResult ListInboundShipmentItemsResult `xml:"ListInboundShipmentItemsByNextTokenResult"`
+	ResponseMetadata               ResponseMetadata               `xml:"ResponseMetadata"`
+}
+
+// ListInboundShipmentItemsResult ...
+type ListInboundShipmentItemsResult struct {
+	ItemData  InboundShipmentItemData `xml:"ItemData"`
+	NextToken string                  `xml:"NextToken"`
+}
+
+// InboundShipmentItemData ...
+type InboundShipmentItemData struct {
+	Members []InboundShipmentItemMember `xml:"member"`
+}
+
+// InboundShipmentItemMember ...
+type InboundShipmentItemMember struct {
+	QuantityShipped string `xml:"QuantityShipped"`
+	ShipmentID      string `xml:"ShipmentId"`
+	PrepDetailsList struct {
+		PrepDetails struct {
+			PrepOwner       string `xml:"PrepOwner"`
+			PrepInstruction string `xml:"PrepInstruction"`
+		} `xml:"PrepDetails"`
+	} `xml:"PrepDetailsList"`
+	FulfillmentNetworkSKU string `xml:"FulfillmentNetworkSKU"`
+	SellerSKU             string `xml:"SellerSKU"`
+	QuantityReceived      string `xml:"QuantityReceived"`
+	QuantityInCase        string `xml:"QuantityInCase"`
+}
