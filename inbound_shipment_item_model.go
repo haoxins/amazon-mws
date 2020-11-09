daabced724d1edb088b81833b846357f8fe0eb2c
haoxin <haoxinst@gmail.com>
2020-11-09 14:00:00 +0800

    split items

--- inbound_shipment_item_model.go
+++ inbound_shipment_item_model.go
@@ -31,16 +31,19 @@ type InboundShipmentItemData struct {
 
 // InboundShipmentItemMember ...
 type InboundShipmentItemMember struct {
-	QuantityShipped string `xml:"QuantityShipped"`
-	ShipmentID      string `xml:"ShipmentId"`
-	PrepDetailsList struct {
-		PrepDetails struct {
-			PrepOwner       string `xml:"PrepOwner"`
-			PrepInstruction string `xml:"PrepInstruction"`
-		} `xml:"PrepDetails"`
-	} `xml:"PrepDetailsList"`
-	FulfillmentNetworkSKU string `xml:"FulfillmentNetworkSKU"`
-	SellerSKU             string `xml:"SellerSKU"`
-	QuantityReceived      string `xml:"QuantityReceived"`
-	QuantityInCase        string `xml:"QuantityInCase"`
+	QuantityShipped       string          `xml:"QuantityShipped"`
+	ShipmentID            string          `xml:"ShipmentId"`
+	PrepDetailsList       PrepDetailsList `xml:"PrepDetailsList"`
+	FulfillmentNetworkSKU string          `xml:"FulfillmentNetworkSKU"`
+	SellerSKU             string          `xml:"SellerSKU"`
+	QuantityReceived      string          `xml:"QuantityReceived"`
+	QuantityInCase        string          `xml:"QuantityInCase"`
+}
+
+// PrepDetailsList ...
+type PrepDetailsList struct {
+	PrepDetails struct {
+		PrepOwner       string `xml:"PrepOwner"`
+		PrepInstruction string `xml:"PrepInstruction"`
+	} `xml:"PrepDetails"`
 }
